81660a878690eb818658b5c385d72983c16458c8
BEN SHAFTO <D3330384@live.tees.ac.uk>
2025-02-18 11:11:43 +0000

    Started implementing service

---
+++ Components/Pages/Scanner/Data/DTOs/GetPackagingInfoDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace EcoEarthPOC.Components.Pages.Scanner.Data.DTOs
+{
+    // Uses the OFF API to get packaging information about a product
+    public class GetPackagingInfoDTO
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("packaging")]
+        public string Packaging { get; set; }
+
+        [JsonProperty("status")]
+        public int Status { get; set; }
+
+        [JsonProperty("status_verbose")]
+        public string StatusVerbose { get; set; }
+    }
+}
---
+++ Components/Services/OFFPackaging.cs
@@ -0,0 +1,54 @@
+﻿using EcoEarthPOC.Components.Pages.Scanner.Data.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+// Service which handles requests to and from the open food facts api
+namespace EcoEarthPOC.Components.Services
+{
+    public class OFFPackaging
+    {
+        // Attatch Barcode number to the end of this for details about product.
+        // E.g. https://en.openfoodfacts.org/api/v0/product/6111035000430.json?fields=packaging
+        const string API_URL = "https://en.openfoodfacts.org/api/v0/product/{0}.json?fields=packaging";
+
+        private readonly HttpClient _httpClient;
+
+        public OFFPackaging(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        JsonSerializerOptions jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        public async Task<GetPackagingInfoDTO> GetPackagingInformation(string productCode)
+        {
+            var url = string.Format(API_URL, productCode);
+
+            var response = await _httpClient.GetAsync(url);
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+
+            var item = JsonSerializer.Deserialize<GetPackagingInfoDTO>(jsonResponse);
+
+            if (item.Status == 0)
+            {
+                // means item does not exist inside api
+            }
+
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(url), "The EventType response is null");
+            }
+
+            return item;
+        }
+
+
+    }
+}
