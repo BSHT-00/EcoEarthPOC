a4166e75b2c7dbc1f622349ed37e99492417e51d
BSHT-00 <benjaminshafto@gmail.com>
2025-03-10 23:00:01 +0000

    Started implementing database. Unsure what other people need for their sections so will need tables creating. Also fixed an error which resulted in the scanner crashing where the materials section was empty. This is fixed; must double check the service is reading the JSON correctly

---
+++ EcoEarth/Backend Stuff/Tables/UserCurrency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoEarthPOC.Backend_Stuff.Tables
+{
+    public class UserCurrency
+    {
+        [Required]
+        private int UserId;
+
+        [Required]
+        private int? Currency;
+    }
+}
