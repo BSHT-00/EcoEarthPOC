fc5e9521e34330b932aa92626b385e8898d6a270
BSHT-00 <benjaminshafto@gmail.com>
2025-03-13 00:26:06 +0000

    dotnet : The specified deps.json [C:\Users\justi\Source\Repos\EcoEarthPOCKY\EcoEarth\bin\Debug\net9.0-android\EcoEarthPOC.deps.json] does not exist At line:1 char:1 + dotnet ef migrations add InitialCreate --output-dir Data/Migrations + ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~     + CategoryInfo          : NotSpecified: (The specified d... does not exist:String) [], RemoteException     + FullyQualifiedErrorId : NativeCommandError
    Fixing this issue

--- EcoEarth/Components/Pages/Scanner/Data/DTOs/IsItRecyclableDTO.cs
+++ EcoEarth/Components/Pages/Scanner/Data/DTOs/IsItRecyclableDTO.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EcoEarthPOC.Components.Pages.Scanner.Data.DTOs
 {
+    // DTO for the IsItRecyclable endpoint. retrieves list of these items from API
     public class IsItRecyclableDTO
     {
+        public string Material { get; set; }
+        public int CategoryId { get; set; }
     }
 }
---
+++ EcoEarth/Components/Services/EcoEarthAPI Services/IsProductRecyclableService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoEarthPOC.Components.Services.EcoEarthAPI_Services
+{
+    public class IsProductRecyclableService
+    {
+        private readonly HttpClient _httpClient;
+
+        public IsProductRecyclableService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> IsProductRecyclableAsync(string productId)
+        {
+            if (string.IsNullOrEmpty(productId))
+            {
+                throw new ArgumentException("Product ID cannot be null or empty", nameof(productId));
+            }
+
+            var response = await _httpClient.GetAsync($"https://api.ecoearthapp.com/recyclable/{productId}");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return bool.Parse(content);
+        }
+    }
+}
