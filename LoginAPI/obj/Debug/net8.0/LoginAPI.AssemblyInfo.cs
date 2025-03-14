c94e2d9ac67db42fb61790c37a47f44978975c74
Ajay Lama <q2043246@live.tees.ac.uk>
2025-03-14 10:26:02 +0000

    Login and registration works

--- LoginAPI/obj/Debug/net8.0/LoginAPI.AssemblyInfo.cs
+++ LoginAPI/obj/Debug/net8.0/LoginAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+247f498da8544d2372169a6e90b8dcc594f24375")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bec46422ac72621fe136088b45ebbd8c3f89a141")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
