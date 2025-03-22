c37736bacc7c2e22f8f7223aa41c5702b78ee9a8
BSHT-00 <benjaminshafto@gmail.com>
2025-03-22 16:46:12 +0000

    Made changes to layout, Created controller which handles the counters for recycling categories

---
+++ EcoEarthAppAPI/Controllers/RecycleCountController.cs
@@ -0,0 +1,85 @@
+﻿using EcoEarthAppAPI.Data;
+using EcoEarthAppAPI.Data.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoEarthAppAPI.Controllers
+{
+    [Route("api/RecycleCount")]
+    [ApiController]
+    public class RecycleCountController : ControllerBase
+    {
+        private readonly EcoEarthAppAPIDbContext _context;
+
+        public RecycleCountController(EcoEarthAppAPIDbContext context)
+        {
+            _context = context;
+        }
+        
+        // Returns the category count for each category
+        [HttpGet("{userId}")]
+        public async Task<RecycleCategoriesDTO> GetRecycleCount(int userId)
+        {
+            var recycleCount = await _context.PastRecycledClassCount
+                .Where(x => x.UserId == userId)
+                .FirstOrDefaultAsync();
+
+            if (recycleCount != null)
+                return new RecycleCategoriesDTO
+                {
+                    Cat1 = recycleCount.Cat1,
+                    Cat2 = recycleCount.Cat2,
+                    Cat3 = recycleCount.Cat3,
+                    Cat4 = recycleCount.Cat4,
+                    Cat5 = recycleCount.Cat5
+                };
+            else
+                return new RecycleCategoriesDTO
+                {
+                    Cat1 = -1,
+                    Cat2 = -1,
+                    Cat3 = -1,
+                    Cat4 = -1,
+                    Cat5 = -1
+                };
+        }
+
+        // Increments value of a category count for a user using categoryId
+        [HttpPost("{userId}/{categoryId}")]
+        public void IncrementCategory(int userId, int categoryId)
+        {
+            var recycleCount = _context.PastRecycledClassCount
+                .Where(x => x.UserId == userId)
+                .FirstOrDefault();
+
+            if (recycleCount == null)
+                return;
+
+            switch (categoryId)
+            {
+                case 1:
+                    recycleCount.Cat1++;
+                    break;
+                case 2:
+                    recycleCount.Cat2++;
+                    break;
+                case 3:
+                    recycleCount.Cat3++;
+                    break;
+                case 4:
+                    recycleCount.Cat4++;
+                    break;
+                case 5:
+                    recycleCount.Cat5++;
+                    break;
+                default:
+                    return;
+            }
+
+            _context.SaveChanges();
+        }
+
+
+    }
+}
