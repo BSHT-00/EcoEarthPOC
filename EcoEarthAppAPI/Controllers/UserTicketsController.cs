0cd7a253d5103b44b1939a8d167b7c66479c411f
BSHT-00 <benjaminshafto@gmail.com>
2025-03-22 14:16:07 +0000

    Added more functionality to ticket system. Ensured alll controllers are commented well

--- EcoEarthAppAPI/Controllers/UserTicketsController.cs
+++ EcoEarthAppAPI/Controllers/UserTicketsController.cs
@@ -16,12 +16,47 @@ namespace EcoEarthAppAPI.Controllers
             _context = context;
         }
 
+        // Gets all tickets
         [HttpGet]
         public async Task<ActionResult<IEnumerable<UserTickets>>> GetUserTickets()
         {
             return await _context.UserTickets.ToListAsync();
         }
 
+        // Gets a ticket by id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserTickets>> GetUserTickets(int id)
+        {
+            var userTickets = await _context.UserTickets.FindAsync(id);
+            if (userTickets == null)
+            {
+                return NotFound();
+            }
+            return userTickets;
+        }
+
+        // Gets incomplete tickets
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserTickets>>> GetIncompleteTickets()
+        {
+            return await _context.UserTickets
+                .Where(x => x.IsCompleted == false)
+                .ToListAsync();
+        }
+
+        // Marks ticket as complete
+        [HttpPut("{ticketId}")]
+        public void MarkTicketComplete(int ticketId)
+        {
+            var userTicket = _context.UserTickets
+                .Where(x => x.TicketId == ticketId)
+                .Where(x => x.IsCompleted == false)
+                .FirstOrDefault();
+            if (userTicket == null)
+                return;
+            userTicket.IsCompleted = true;
+            _context.SaveChanges();
+        }
 
         // Posts a new ticket
         [HttpPost]
---
+++ EcoEarthAppAPI/Data/DTOs/RecycleCategoriesDTO.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcoEarthAppAPI.Data.DTOs
+{
+    public class RecycleCategoriesDTO
+    {
+        // Plastic
+        [Range(0, int.MaxValue)]
+        public int Cat1 { get; set; }
+
+
+        // Glass
+        [Range(0, int.MaxValue)]
+        public int Cat2 { get; set; }
+
+
+        // Metal
+        [Range(0, int.MaxValue)]
+        public int Cat3 { get; set; }
+
+
+        // Paper
+        [Range(0, int.MaxValue)]
+        public int Cat4 { get; set; }
+
+        // Cardboard
+        [Range(0, int.MaxValue)]
+        public int Cat5 { get; set; }
+    }
+}
