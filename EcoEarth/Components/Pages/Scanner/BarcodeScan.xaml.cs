7ed50fce7f68c15a5d374cce09af8fbc4e6ed982
BSHT-00 <benjaminshafto@gmail.com>
2025-03-23 16:15:44 +0000

    Layout changes, further implemented the barcode scanner

--- EcoEarth/Components/Pages/Scanner/BarcodeScan.xaml.cs
+++ EcoEarth/Components/Pages/Scanner/BarcodeScan.xaml.cs
@@ -5,14 +5,14 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using Microsoft.AspNetCore.Http.Extensions;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcoEarthPOC.Components.Pages.Scanner;
 
 public partial class BarcodeScanTest : ContentPage
 {
-    [Inject]
-    private NavigationManager _NavigationManager { get; set; }
-
     public BarcodeScanTest()
     {
         InitializeComponent();
@@ -29,26 +29,23 @@ public partial class BarcodeScanTest : ContentPage
     private void CameraBarcodeReaderView_BarcodesDetected(object sender, BarcodeDetectionEventArgs e)
     {
         // Ensure the event is handled on the main thread
-        MainThread.BeginInvokeOnMainThread(() =>
+        MainThread.BeginInvokeOnMainThread(async () =>
         {
             var result = e.Results[0].Value.ToString();
 
-            if (result == null || _NavigationManager == null)
+            if (result == null)
                 return;
             // Validating that the barcode is in the valid format
             if (validateBarcodeNumber(result))
             {
 
-                // If so, take user to product info page
-                _NavigationManager.NavigateTo("/scanner/productinfo/" + e.Results[0].Value.ToString());
+                // If so, take user to product info page (scanner/scanner/productinfo/{result})
             }
         });
     }
-
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
-        if (_NavigationManager != null)
-            _NavigationManager.NavigateTo("scanner/scanner");
+        // redirect back to scanner page
     }
 
     protected bool validateBarcodeNumber(string barcode)
@@ -57,4 +54,5 @@ public partial class BarcodeScanTest : ContentPage
         var regex = new System.Text.RegularExpressions.Regex(@"^(?:\d{8}|\d{12}|\d{13}|\d{6})$");
         return regex.IsMatch(barcode);
     }
+
 }
