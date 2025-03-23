2be8a20d72d4f9badd88b460c6fedd58df556420
BSHT-00 <benjaminshafto@gmail.com>
2025-03-23 00:45:38 +0000

    Enhance barcode scanning UI and navigation features
    Updated `BarcodeScan.xaml` with new layout and user instructions.
    Injected `NavigationManager` in `BarcodeScan.xaml.cs` for improved navigation and modified barcode detection logic.
    Added button styles in CSS and included new Maui assets in the project file.
    Adjusted `AndroidManifest.xml` for permissions and added `EcoEarthIcon.ico`.

--- EcoEarth/Components/Pages/Scanner/BarcodeScan.xaml.cs
+++ EcoEarth/Components/Pages/Scanner/BarcodeScan.xaml.cs
@@ -1,9 +1,14 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using ZXing.Net.Maui;
 
 namespace EcoEarthPOC.Components.Pages.Scanner;
 
 public partial class BarcodeScanTest : ContentPage
 {
+    [Inject]
+    public NavigationManager? NavigationManager { get; set; }
+
     public BarcodeScanTest()
     {
         InitializeComponent();
@@ -21,12 +26,25 @@ public partial class BarcodeScanTest : ContentPage
         // Ensure the event is handled on the main thread
         MainThread.BeginInvokeOnMainThread(() =>
         {
-            // Update the label with the detected barcode value
-            if (e.Results.Any())
+            if (e.Results == null)
+                return;
+            if (validateBarcodeNumber(e.Results.ToString()))
             {
-                barcodeResult.Text = e.Results.First().Value;
-                Console.WriteLine(barcodeResult.Text);
+                NavigationManager.NavigateTo("/scanner/productinfo/" + e.Results.ToString());
             }
         });
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        if (NavigationManager != null)
+            NavigationManager.NavigateTo("scanner/scanner");
+    }
+
+    protected bool validateBarcodeNumber(string barcode)
+    {
+        // Validate barcode using reg exp
+        var regex = new System.Text.RegularExpressions.Regex(@"^(?:\d{8}|\d{12}|\d{13}|\d{6})$");
+        return regex.IsMatch(barcode);
     }
 }
