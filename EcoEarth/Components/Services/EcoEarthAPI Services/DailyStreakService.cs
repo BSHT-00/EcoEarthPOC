8a88915431eb0f7a9095f913c2bd5353d9aa6388
S3028837 <S3028837@live.tees.ac.uk>
2025-04-17 17:38:37 +0100

    updated Daily Streak display

---
+++ EcoEarth/Components/Services/EcoEarthAPI Services/DailyStreakService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EcoEarthPOC.Components.Services.EcoEarthAPI_Services
+{
+    //handles dailyStreak logic
+    class DailyStreakService
+    {
+        //https://localhost:7111/api/DailyStreak
+
+        // Base URL that changes based on platform. Android uses 10.0.2.2:<port> to access localhost api
+        public static string ServiceBaseUrl = DeviceInfo.Platform == DevicePlatform.Android ? "https://10.0.2.2:7111/api" : "https://localhost:7111/api";
+        const string Endpoint = "/DailyStreak";
+        private readonly HttpClient _httpClient;
+
+        //httpClient bypasss
+        public DailyStreakService(HttpClient httpClient)
+        {
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            _httpClient = new HttpClient(handler);
+        }
+
+        JsonSerializerOptions jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        //Get users streak
+        public async Task<int> GetUserStreak()
+        {
+            var response = await _httpClient.GetAsync($"{ServiceBaseUrl}{Endpoint}/{AppVariables.UserId}");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<int>(content, jsonOptions);
+            }
+            return 0;
+        }
+
+        //adds to users streak
+        public async Task AddToStreak(int day)
+        {
+            var url = $"{ServiceBaseUrl}{Endpoint}/{AppVariables.UserId}/{day}";
+
+            var response = await _httpClient.PutAsync(url, null);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Failed to add day to daily streak");
+            }
+        }
+
+        //remove users streak
+        public async Task RemoveStreak()
+        {
+            var url = $"{ServiceBaseUrl}{Endpoint}/{AppVariables.UserId}";
+
+            var response = await _httpClient.DeleteAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Failed to remove daily streak");
+            }
+        }
+    }
+}
+    
